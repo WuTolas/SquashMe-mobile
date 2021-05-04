facd107f1496f27ca3e81d9cd2f6465c7da89f6b
WuTolas <wut1337@hotmail.com>
2021-05-04 12:01:46 +0200

    #4 quick match init form screen

--- app/src/main/java/pl/pjatk/squashme/MainActivity.java
+++ app/src/main/java/pl/pjatk/squashme/MainActivity.java
@@ -2,7 +2,9 @@ package pl.pjatk.squashme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button quickMatchButton = findViewById(R.id.quick_match);
+        quickMatchButton.setOnClickListener(V -> launchActivity(QuickMatchActivity.class));
+    }
+
+    private void launchActivity(Class<? extends AppCompatActivity> activity) {
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/pl/pjatk/squashme/QuickMatchActivity.java
@@ -0,0 +1,14 @@
+package pl.pjatk.squashme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuickMatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_match);
+    }
+}
