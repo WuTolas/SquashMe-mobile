3414e4fc6296cd965e54513cb64c47032fba930b
Piotr <piotr.krajewski89@gmail.com>
2021-05-06 21:52:36 +0200

    update creating referee fragment with params

--- app/src/main/java/pl/pjatk/squashme/RefereeFragment.kt
+++ app/src/main/java/pl/pjatk/squashme/RefereeFragment.kt
@@ -1,56 +1,75 @@
 package pl.pjatk.squashme
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val PLAYER_ONE_NAME: String = "paramNameOne"
+private const val PLAYER_TWO_NAME: String = "paramNameTwo"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RefereeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RefereeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var playerOneName: String? = null
+    private var playerTwoName: String? = null
+    private var playerOneScore: Int = 0
+    private var playerTwoScore: Int = 0
+    private var playerOneSet: Int = 0
+    private var playerTwoSet: Int = 0
+    private lateinit var playerOneNameHolder: Button
+    private lateinit var playerTwoNameHolder: Button
+    private lateinit var playerOneScoreBtn: Button
+    private lateinit var playerTwoScoreBtn: Button
+    private lateinit var playerOneSetNumber: TextView
+    private lateinit var playerTwoSetNumber: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            playerOneName = it.getString(PLAYER_ONE_NAME)
+            playerTwoName = it.getString(PLAYER_TWO_NAME)
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_referee, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+        playerOneNameHolder.text = playerOneName
+        playerTwoNameHolder.text = playerTwoName
+        playerOneScoreBtn.text = playerOneScore.toString()
+        playerTwoScoreBtn.text = playerTwoScore.toString()
+        playerOneSetNumber.text = playerOneSet.toString()
+        playerTwoSetNumber.text = playerTwoSet.toString()
+    }
+
+    private fun init(view: View) {
+        playerOneNameHolder = view.findViewById(R.id.player_one_name)
+        playerTwoNameHolder = view.findViewById(R.id.player_two_name)
+        playerOneScoreBtn = view.findViewById(R.id.player_one_add_point_btn)
+        playerTwoScoreBtn = view.findViewById(R.id.player_two_add_point_btn)
+        playerOneSetNumber = view.findViewById(R.id.player_one_set)
+        playerTwoSetNumber = view.findViewById(R.id.player_two_set)
     }
 
     companion object {
         /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
+         * @param playerOneName Parameter 1.
+         * @param playerTwoName Parameter 2.
          * @return A new instance of fragment RefereeFragment.
          */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(playerOneName: String, playerTwoName: String) =
                 RefereeFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+                        putString(PLAYER_ONE_NAME, playerOneName)
+                        putString(PLAYER_TWO_NAME, playerTwoName)
                     }
                 }
     }
