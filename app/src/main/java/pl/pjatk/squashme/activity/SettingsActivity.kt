bee7600005e5423a63c772431fe3842cc61c28c5
Piotr <piotr.krajewski89@gmail.com>
2021-06-10 00:00:14 +0200

    change language name to display

--- app/src/main/java/pl/pjatk/squashme/activity/SettingsActivity.kt
+++ app/src/main/java/pl/pjatk/squashme/activity/SettingsActivity.kt
@@ -7,6 +7,8 @@ import android.widget.AdapterView
 import android.widget.Spinner
 import pl.pjatk.squashme.R
 import pl.pjatk.squashme.config.LocaleUtil
+import pl.pjatk.squashme.model.Language
+import java.util.*
 
 class SettingsActivity : BaseActivity() {
 
@@ -26,8 +28,9 @@ class SettingsActivity : BaseActivity() {
         languages.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 val selectedItem = languages.selectedItem.toString()
-                if (selectedItem != storage.getPreferredLocale()) {
-                    updateAppLocale(languages.selectedItem.toString())
+                val langCode = getLanguageCode(selectedItem)
+                if (langCode != storage.getPreferredLocale()) {
+                    updateAppLocale(langCode)
                     recreate()
                 }
             }
@@ -40,11 +43,16 @@ class SettingsActivity : BaseActivity() {
         val adapter = languages.adapter
         val n = adapter.count
         for (i in 0 until n) {
-            if (adapter.getItem(i) == storage.getPreferredLocale()) {
+            val code = getLanguageCode(adapter.getItem(i))
+            if (code == storage.getPreferredLocale()) {
                 languages.setSelection(i)
                 break
             }
         }
+    }
+
+    private fun getLanguageCode(language: Any): String {
+        return Language.valueOf(language.toString().toUpperCase(Locale.getDefault())).code
     }
 
     private fun updateAppLocale(locale: String) {
---
+++ app/src/main/java/pl/pjatk/squashme/model/Language.kt
@@ -0,0 +1,6 @@
+package pl.pjatk.squashme.model
+
+enum class Language(val code: String) {
+    ENGLISH("en"),
+    POLSKI("pl")
+}
