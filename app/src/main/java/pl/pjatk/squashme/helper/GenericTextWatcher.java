5a2b015b17ae478b9ba6d07749c70e26d7aeb62e
WuTolas <wut1337@hotmail.com>
2021-06-16 21:23:09 +0200

    #58 create quick match fix

---
+++ app/src/main/java/pl/pjatk/squashme/helper/GenericTextWatcher.java
@@ -0,0 +1,33 @@
+package pl.pjatk.squashme.helper;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+/**
+ * Generic text watcher for TextInputLayout
+ */
+public class GenericTextWatcher implements TextWatcher {
+
+    private final TextInputLayout inputLayout;
+
+    public GenericTextWatcher(TextInputLayout inputLayout) {
+        this.inputLayout = inputLayout;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        inputLayout.setErrorEnabled(false);
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
